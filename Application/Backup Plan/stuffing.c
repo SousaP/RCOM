c6212dc6f784044c3e05bb45acfa6ec0898caf86
Psouka <p4so@gmail.com>
2014-10-30 03:00:04 +0000

    unsigned ;__;

--- Application/Backup Plan/stuffing.c
+++ Application/Backup Plan/stuffing.c
@@ -4,7 +4,7 @@
 int stuffing(char* data, int size, char* stuffed){
     int i = 0;
     int j = 0;
-    char byte;
+    unsigned char byte;
 
     while(i < size){
         byte = data[i];
@@ -46,7 +46,7 @@ int stuffing(char* data, int size, char* stuffed){
 int unstuffing(char* stuffed, int size, char* data){
     int i = 0;
     int j = 0;
-    char byte;
+    unsigned char byte;
 
     while(i < size){
 
